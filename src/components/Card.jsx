bd331b42c6a991726b22c15d2ac57866445998fc
Robert Butler <robertbutler@protonmail.com>
2022-02-23 14:45:58 +0000

    feat: add Card.jsx component and styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,21 @@
 import '../styles/card.scss';
 
 export default function Card() {
+  const equilibrium = {
+    title: 'Equilibrium #3429',
+    src: './assets/image-equilibrium.jpg',
+    desc: 'Our Equilibrium collection prokotes balance and calm.',
+  };
+
   return (
     <div className='card'>
       <img
-        className='nft'
-        src='./public/assets/image-equilibrium.jpg' // NOT LOADING?!
+        className='nft image'
+        src={equilibrium.src}
         alt='An NFT named Equilibrium by Jules Wyvern'
       />
+      <h1 className='nft title'>{equilibrium.title}</h1>
+      <p className='nft body'>{equilibrium.desc}</p>
     </div>
   );
 }
