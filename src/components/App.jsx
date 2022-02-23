1ad80b67fb714371ab124363bcae7311fae58a1c
Robert Butler <robertbutler@protonmail.com>
2022-02-23 16:59:01 +0000

    feat: add attributions to app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,25 @@ export default function App() {
   return (
     <div className='container'>
       <Card />
+      <p className='footer-body'>
+        Challenge by{' '}
+        <a
+          className='footer-link'
+          href='https://www.frontendmentor.io/home'
+          target={'_blank'}
+        >
+          Frontend Mentor
+        </a>
+        . Coded by{' '}
+        <a
+          className='footer-link'
+          href='https://github.com/chekhovs-func'
+          target={'_blank'}
+        >
+          Robert Butler
+        </a>
+        .{' '}
+      </p>
     </div>
   );
 }
